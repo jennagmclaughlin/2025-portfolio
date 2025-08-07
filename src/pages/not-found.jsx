48ad2edfa94db3e74a3259d0418ff29403916458
Jenna McLaughlin <89791791+jennagmclaughlin@users.noreply.github.com>
2025-08-07 18:11:24 -0400

    added star background to 404 page

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -1,3 +1,10 @@
+import { StarBackground } from "@/components/star-background";
+
 export const NotFound = () => {
-  return <div className="min-h-screen">404 not found</div>;
+  return (
+    <div className="min-h-screen">
+      404 not found
+      <StarBackground />
+    </div>
+  );
 };
