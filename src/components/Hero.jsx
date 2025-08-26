2af2ca81777f3a82f34b7a806697421cbcbb086e
Jenna McLaughlin <89791791+jennagmclaughlin@users.noreply.github.com>
2025-08-26 01:05:10 -0400

    fixed dependencies

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion, useScroll, useTransform } from "motion/react";
 import { StarBackground } from "./StarBackground";
 import { StarCloud } from "./StarCloud";
 
